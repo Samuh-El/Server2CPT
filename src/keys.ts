46aa434e1e4f6469ed153ae47518adf28c551b34
= <samuelgajardo@outlook.com>
2020-07-01 00:05:48 -0400

    jhasd

--- src/keys.ts
+++ src/keys.ts
@@ -3,17 +3,17 @@ export default{
 
           //datos de conexion a db
 
-          // host:'190.107.177.34',
-          // user:'producto_Felipe', 
-          // password:']wq-8xIFvf26',
-          // database:'producto_chile',  
+          host:'culturaparatodos.cl',
+          user:'culturap_Felipe', 
+          password:']wq-8xIFvf26',
+          database:'culturap_cultura_para_todos' 
           
           //datos de conexion a localhost
           
-          host:'localhost',
-          user:'root', 
-          password:'',
-          database:'cultura_para_todos',
+          // host:'localhost',
+          // user:'root', 
+          // password:'',
+          // database:'cultura_para_todos',
      }   
         
 }
