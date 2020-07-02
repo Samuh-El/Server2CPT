e64610f8ac59bcb2c10f9861c677911ca2bd4357
= <samuelgajardo@outlook.com>
2020-07-02 18:25:40 -0400

    ajsdhasd

--- src/controllers/appController.ts
+++ src/controllers/appController.ts
@@ -93,6 +93,10 @@ class AppController {
      public async getInfoEspectaculos(req: Request, res: Response) {
           const data = await pool.query('SELECT e.nombreEspectaculo as nombre,e.descripcionEspectaculo as descripcionCompleta,e.desdeHorario as horaInicio,e.hastaHorario as horaTermino,DATE_FORMAT(e.fechaEspectaculo,\'%d/%m/%Y\') as fecha,e.descripcionResumida,e.valor as precio,e.rutaImagenBanner as rutaBanner,e.rutaImagenAfiche as rutaAfiche,t.nombreTipo as tipoEspectaculo,o.nombreOrganizador as organizador,a.nombreArtistas as artista FROM `espectaculo` e INNER JOIN `tipoespectaculo` t ON e.tipoEspectaculo_idTipoEspectaculo = t.idTipoEspectaculo INNER JOIN `organizador` o ON e.organizador_idOrganizador = o.idOrganizador INNER JOIN `artistas` a ON e.artistas_idArtistas = a.idArtistas WHERE e.visible = 1');
           res.json(data);
+          if (data.lenth > 0) {
+               return res.json(data);
+          } 
+          res.status(404).json({ text: "no retorna nada" });
      } 
 }
 
